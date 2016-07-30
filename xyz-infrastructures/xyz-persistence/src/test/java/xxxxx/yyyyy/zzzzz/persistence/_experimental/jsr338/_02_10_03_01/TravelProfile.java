de63f724ca573fc53457d7570126c27a3fbdfc02
Masamitsu Namioka <masamitsunamioka@gmail.com>
2016-07-30 15:26:17 +0900

    JSR338 2.10 Relationship Mapping Defaults

---
+++ xyz-infrastructures/xyz-persistence/src/test/java/xxxxx/yyyyy/zzzzz/persistence/_experimental/jsr338/_02_10_03_01/TravelProfile.java
@@ -0,0 +1,15 @@
+package xxxxx.yyyyy.zzzzz.persistence._experimental.jsr338._02_10_03_01;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@lombok.NoArgsConstructor
+@lombok.AllArgsConstructor
+@lombok.Data
+@Entity
+public class TravelProfile implements Serializable {
+
+    @Id
+    private Long id;
+}
---
+++ xyz-infrastructures/xyz-persistence/src/test/java/xxxxx/yyyyy/zzzzz/persistence/_experimental/jsr338/_02_10_05_01/UnidirectionalOneToManyRelationshipsTest.java
@@ -0,0 +1,24 @@
+package xxxxx.yyyyy.zzzzz.persistence._experimental.jsr338._02_10_05_01;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import org.junit.Test;
+
+public class UnidirectionalOneToManyRelationshipsTest {
+
+    @Test
+    public void test() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("_02_10_05_01");
+        EntityManager em = emf.createEntityManager();
+        em.close();
+        emf.close();
+//
+// CREATE TABLE EMPLOYEE (ID BIGINT NOT NULL, PRIMARY KEY (ID))
+// CREATE TABLE ANNUALREVIEW (ID BIGINT NOT NULL, PRIMARY KEY (ID))
+// CREATE TABLE EMPLOYEE_ANNUALREVIEW (Employee_ID BIGINT NOT NULL, annualReviews_ID BIGINT NOT NULL, PRIMARY KEY (Employee_ID, annualReviews_ID))
+// ALTER TABLE EMPLOYEE_ANNUALREVIEW ADD CONSTRAINT FK_EMPLOYEE_ANNUALREVIEW_annualReviews_ID FOREIGN KEY (annualReviews_ID) REFERENCES ANNUALREVIEW (ID)
+// ALTER TABLE EMPLOYEE_ANNUALREVIEW ADD CONSTRAINT FK_EMPLOYEE_ANNUALREVIEW_Employee_ID FOREIGN KEY (Employee_ID) REFERENCES EMPLOYEE (ID)
+//
+    }
+}
