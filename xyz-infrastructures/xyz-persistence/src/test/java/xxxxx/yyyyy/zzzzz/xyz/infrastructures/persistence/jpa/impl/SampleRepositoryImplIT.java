edfe42b5bbf9020bf762c8b725c5db8b3046ed43
Masamitsu Namioka <masamitsunamioka@gmail.com>
2016-09-17 22:59:27 +0900

    issues/#51-Try-bean-managed-transactions

--- xyz-infrastructures/xyz-persistence/src/test/java/xxxxx/yyyyy/zzzzz/xyz/infrastructures/persistence/jpa/impl/SampleRepositoryImplIT.java
+++ xyz-infrastructures/xyz-persistence/src/test/java/xxxxx/yyyyy/zzzzz/xyz/infrastructures/persistence/jpa/impl/SampleRepositoryImplIT.java
@@ -1,14 +1,22 @@
 package xxxxx.yyyyy.zzzzz.xyz.infrastructures.persistence.jpa.impl;
 
-import static java.util.stream.Collectors.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
 
 import java.io.File;
 import java.util.List;
 import java.util.stream.Stream;
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.Status;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.arquillian.transaction.api.annotation.Transactional;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
@@ -16,9 +24,7 @@ import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.TestName;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import xxxxx.yyyyy.zzzzz.xyz.domain.model.Sample;
@@ -31,19 +37,13 @@ import xxxxx.yyyyy.zzzzz.xyz.infrastructures.persistence.POM;
 public class SampleRepositoryImplIT {
 
     @Inject
+    private UserTransaction userTransaction;
+    @Inject
+    private EntityManager entityManager;
+    @Inject
     private SampleRepository sampleRepository;
-    @Rule
-    public TestName name = new TestName();
-
-    @Before
-    public void before() {
-        log.info(String.format("[START] %s", name.getMethodName()));
-    }
-
-    @After
-    public void after() {
-        log.info("[END]");
-    }
+    //@Rule
+    //public TestName name = new TestName();
 
     @Deployment
     public static WebArchive createDeployment() {
@@ -62,7 +62,6 @@ public class SampleRepositoryImplIT {
                 .addAsResource("META-INF/beans.xml", "META-INF/beans.xml")
                 .addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
                 .addAsResource("logback.xml", "logback.xml");
-        //.addAsResource("logging.properties", "logging.properties");
         war.addAsLibraries(jar);
         if (log.isDebugEnabled()) {
             log.debug(war.toString(true));
@@ -70,101 +69,153 @@ public class SampleRepositoryImplIT {
         return war;
     }
 
-    @Transactional
+    @Before
+    public void before() {
+        //log.info(String.format("[START] %s", name.getMethodName()));
+        this.begin();
+        this.entityManager.createQuery("delete from Sample").executeUpdate();
+        this.commit();
+    }
+
+    @After
+    public void after() {
+        //log.info("[END]");
+        this.begin();
+        this.entityManager.createQuery("delete from Sample").executeUpdate();
+        this.commit();
+    }
+
+    //@Transactional
     @Test(expected = IllegalArgumentException.class)
     public void UnitOfWork_StateUnderTest_ExpectedBehavior0() { // TODO should change method name
         sampleRepository.findByName(null);
     }
 
-    @Transactional
-    //@Test
+    //@Transactional
+    @Test
     public void UnitOfWork_StateUnderTest_ExpectedBehavior1() { // TODO should change method name
-        testData(5L);
+        prepareData(5L);
         List<Sample> samples = sampleRepository.findAll();
-        samples.stream().forEach(x -> {
-            if (log.isDebugEnabled()) {
-                log.debug(x.toString());
-            }
-        });
+        assertThat("", samples.size(), equalTo(5));
     }
 
-    @Transactional
-    //@Test
+    //@Transactional
+    @Test
     public void UnitOfWork_StateUnderTest_ExpectedBehavior2() { // TODO should change method name
-        testData(10L);
+        prepareData(10L);
         Sample x = sampleRepository.findByName("Name1");
-        if (log.isDebugEnabled()) {
-            log.debug(x.toString());
-        }
+        assertThat("", x.id(), equalTo(1L));
+        assertThat("", x.version(), equalTo(1L));
+        assertThat("", x.name(), equalTo("Name1"));
     }
 
-    @Transactional
-    //@Test
-    public void UnitOfWork_StateUnderTest_ExpectedBehavior3() { // TODO should change method name
-        sampleRepository.bulkUpdateNameByIds("Hoge", Stream.of(1L, 2L).collect(toList()));
-//        sampleRepository.bulkUpdateNameByIds("Hoge", Collections.emptyList());
-//javax.persistence.PersistenceException: Exception [EclipseLink-4002] (Eclipse Persistence Services - 2.5.2.v20140319-9ad6abd): org.eclipse.persistence.exceptions.DatabaseException
-//Internal Exception: java.sql.SQLSyntaxErrorException: Syntax error: Encountered ")" at line 1, column 72.
-//Error Code: 20000
-//Call: UPDATE SAMPLE SET VERSION = (VERSION + 1), NAME = 'Hoge' WHERE (ID IN ())
-//Query: UpdateAllQuery(referenceClass=Sample sql="UPDATE SAMPLE SET VERSION = (VERSION + ?), NAME = ? WHERE (ID IN ())")
-// :
-// :
-        List<Sample> samples = sampleRepository.findAll();
-        samples.stream().forEach(x -> {
-            if (log.isDebugEnabled()) {
-                log.debug(x.toString());
-            }
+//    //@Transactional
+//    //@Test
+//    public void UnitOfWork_StateUnderTest_ExpectedBehavior3() { // TODO should change method name
+//        sampleRepository.bulkUpdateNameByIds("Hoge", Stream.of(1L, 2L).collect(toList()));
+////
+////        sampleRepository.bulkUpdateNameByIds("Hoge", Collections.emptyList());
+////
+////javax.persistence.PersistenceException: Exception [EclipseLink-4002] (Eclipse Persistence Services - 2.5.2.v20140319-9ad6abd): org.eclipse.persistence.exceptions.DatabaseException
+////Internal Exception: java.sql.SQLSyntaxErrorException: Syntax error: Encountered ")" at line 1, column 72.
+////Error Code: 20000
+////Call: UPDATE SAMPLE SET VERSION = (VERSION + 1), NAME = 'Hoge' WHERE (ID IN ())
+////Query: UpdateAllQuery(referenceClass=Sample sql="UPDATE SAMPLE SET VERSION = (VERSION + ?), NAME = ? WHERE (ID IN ())")
+//// :
+//// :
+//        List<Sample> samples = sampleRepository.findAll();
+//        samples.stream().forEach(x -> {
+//            if (log.isDebugEnabled()) {
+//                log.debug(x.toString());
+//            }
+//        });
+//    }
+//
+//    //@Transactional
+//    //@Test
+//    public void UnitOfWork_StateUnderTest_ExpectedBehavior4() { // TODO should change method name
+//        sampleRepository.bulkDeleteByIds(Stream.of(1L, 2L).collect(toList()));
+////
+////        sampleRepository.bulkDeleteByIds(Collections.emptyList());
+////
+////javax.persistence.PersistenceException: Exception [EclipseLink-4002] (Eclipse Persistence Services - 2.5.2.v20140319-9ad6abd): org.eclipse.persistence.exceptions.DatabaseException
+////Internal Exception: java.sql.SQLSyntaxErrorException: Syntax error: Encountered ")" at line 1, column 34.
+////Error Code: 20000
+////Call: DELETE FROM SAMPLE WHERE (ID IN ())
+////Query: DeleteAllQuery(referenceClass=Sample sql="DELETE FROM SAMPLE WHERE (ID IN ())")
+//// :
+//// :
+//        List<Sample> samples = sampleRepository.findAll();
+//        samples.stream().forEach(x -> {
+//            if (log.isDebugEnabled()) {
+//                log.debug(x.toString());
+//            }
+//        });
+//    }
+//
+//    //@Transactional
+//    //@Test
+//    public void UnitOfWork_StateUnderTest_ExpectedBehavior5() { // TODO should change method name
+//        if (log.isDebugEnabled()) {
+//            log.debug(sampleRepository.find(1L).toString());
+//        }
+//        //sampleRepository.bulkDeleteByIds(Stream.of(1L, 2L).collect(toList()));
+//        sampleRepository.bulkUpdateNameByIds("Hoge", Stream.of(1L, 2L).collect(toList()));
+//        if (log.isDebugEnabled()) {
+//            log.debug(sampleRepository.find(1L).toString());
+//        }
+//    }
+//
+//    //@Transactional
+//    //@Test
+//    public void UnitOfWork_StateUnderTest_ExpectedBehavior6() { // TODO should change method name
+//        if (log.isDebugEnabled()) {
+//            log.debug(sampleRepository.find(1L).toString());
+//        }
+//    }
+//
+    private void prepareData(long size) {
+        this.begin();
+        Stream.iterate(1L, x -> x + 1).limit(size).forEach(x -> {
+            this.entityManager.persist(new Sample(x, String.format("Name%d", x)));
         });
+        this.commit();
     }
 
-    @Transactional
-    //@Test
-    public void UnitOfWork_StateUnderTest_ExpectedBehavior4() { // TODO should change method name
-        sampleRepository.bulkDeleteByIds(Stream.of(1L, 2L).collect(toList()));
-//        sampleRepository.bulkDeleteByIds(Collections.emptyList());
-//javax.persistence.PersistenceException: Exception [EclipseLink-4002] (Eclipse Persistence Services - 2.5.2.v20140319-9ad6abd): org.eclipse.persistence.exceptions.DatabaseException
-//Internal Exception: java.sql.SQLSyntaxErrorException: Syntax error: Encountered ")" at line 1, column 34.
-//Error Code: 20000
-//Call: DELETE FROM SAMPLE WHERE (ID IN ())
-//Query: DeleteAllQuery(referenceClass=Sample sql="DELETE FROM SAMPLE WHERE (ID IN ())")
-// :
-// :
-        List<Sample> samples = sampleRepository.findAll();
-        samples.stream().forEach(x -> {
-            if (log.isDebugEnabled()) {
-                log.debug(x.toString());
+    private void begin() {
+        try {
+            this.userTransaction.begin();
+            this.entityManager.joinTransaction();
+        } catch (NotSupportedException | SystemException cause) {
+            throw new RuntimeException(cause);
+        } finally {
+        }
+    }
+
+    private void commit() {
+        try {
+            // TODO
+            if (this.userTransaction.getStatus() != Status.STATUS_ACTIVE) {
+                throw new IllegalStateException(String.format("Illegal status=%d", this.userTransaction.getStatus()));
             }
-        });
-    }
-
-    @Transactional
-    //@Test
-    public void UnitOfWork_StateUnderTest_ExpectedBehavior5() { // TODO should change method name
-        if (log.isDebugEnabled()) {
-            log.debug(sampleRepository.find(1L).toString());
-        }
-        //sampleRepository.bulkDeleteByIds(Stream.of(1L, 2L).collect(toList()));
-        sampleRepository.bulkUpdateNameByIds("Hoge", Stream.of(1L, 2L).collect(toList()));
-        if (log.isDebugEnabled()) {
-            log.debug(sampleRepository.find(1L).toString());
+            this.userTransaction.commit();
+            this.entityManager.clear();
+            this.entityManager.getEntityManagerFactory().getCache().evictAll();
+        } catch (RollbackException | HeuristicMixedException | HeuristicRollbackException | SystemException cause) {
+            throw new RuntimeException(cause);
+        } finally {
         }
     }
 
-    @Transactional
-    //@Test
-    public void UnitOfWork_StateUnderTest_ExpectedBehavior6() { // TODO should change method name
-        if (log.isDebugEnabled()) {
-            log.debug(sampleRepository.find(1L).toString());
+    private void rollback() {
+        try {
+            // TODO
+            this.userTransaction.rollback();
+            this.entityManager.clear();
+            this.entityManager.getEntityManagerFactory().getCache().evictAll();
+        } catch (SystemException cause) {
+            throw new RuntimeException(cause);
+        } finally {
         }
-    }
-
-    private void testData(long size) {
-        Stream.iterate(1L, i -> i + 1).limit(size).forEach(i -> {
-            Sample sample = sampleRepository.find(i);
-            if (sample == null) {
-                sampleRepository.store(new Sample(i, "Name" + i));
-            }
-        });
     }
 }
