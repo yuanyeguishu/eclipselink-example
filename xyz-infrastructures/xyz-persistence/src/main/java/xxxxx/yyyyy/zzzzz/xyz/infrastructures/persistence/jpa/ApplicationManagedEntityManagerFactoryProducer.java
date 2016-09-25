f5f012c76bfe8e16dcd2549f0edf5f1ba5b6cc98
Masamitsu Namioka <masamitsunamioka@gmail.com>
2016-09-25 12:56:42 +0900

    Revert "Merge pull request #59 from namioka/issues/#58-EntityManagers"
    This reverts commit f8885ee80a9b511269993600c210dc2ca1ed62bb, reversing
    changes made to 9d440d2023b5259c3c46b85773d787d1de9a4829.

---
+++ xyz-infrastructures/xyz-persistence/src/main/java/xxxxx/yyyyy/zzzzz/xyz/infrastructures/persistence/jpa/ApplicationManagedEntityManagerFactoryProducer.java
@@ -0,0 +1,33 @@
+package xxxxx.yyyyy.zzzzz.xyz.infrastructures.persistence.jpa;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Disposes;
+import javax.enterprise.inject.Produces;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+@lombok.extern.slf4j.Slf4j
+//@ApplicationScoped
+public class ApplicationManagedEntityManagerFactoryProducer {
+
+    private static final String PERSISTENCE_UNIT_NAME = "xyz_PU"; // TODO inject
+
+    @ApplicationScoped
+    @Produces
+    public EntityManagerFactory produce() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        if (log.isTraceEnabled()) {
+            log.trace(String.format("produce -> %s", emf.toString()));
+        }
+        return emf;
+    }
+
+    public void dispose(@Disposes EntityManagerFactory emf) {
+        if (log.isTraceEnabled()) {
+            log.trace(String.format("dispose -> %s", emf.toString()));
+        }
+        if (emf != null && emf.isOpen()) {
+            emf.close();
+        }
+    }
+}
--- xyz-infrastructures/xyz-persistence/src/main/java/xxxxx/yyyyy/zzzzz/xyz/infrastructures/persistence/jpa/ContainerManagedEntityManagerProducer.java
+++ xyz-infrastructures/xyz-persistence/src/main/java/xxxxx/yyyyy/zzzzz/xyz/infrastructures/persistence/jpa/ContainerManagedEntityManagerProducer.java
@@ -1,35 +1,36 @@
-//package xxxxx.yyyyy.zzzzz.xyz.infrastructures.persistence.jpa;
-//
-//import javax.enterprise.context.RequestScoped;
-//import javax.enterprise.inject.Disposes;
-//import javax.enterprise.inject.Produces;
-//import javax.persistence.EntityManager;
-//import javax.persistence.PersistenceContext;
-//
-//@lombok.extern.slf4j.Slf4j
-//@RequestScoped
-//public class ContainerManagedEntityManagerProducer {
-//
-//    @PersistenceContext //(unitName = PERSISTENCE_UNIT_NAME)
-//    private EntityManager em;
-//
-//    @RequestScoped
-//    @Produces
-//    public EntityManager produce() {
-//        if (log.isTraceEnabled()) {
-//            log.trace(String.format("@Produces -> %s", this.em.toString()));
+package xxxxx.yyyyy.zzzzz.xyz.infrastructures.persistence.jpa;
+
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Disposes;
+import javax.enterprise.inject.Produces;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@lombok.extern.slf4j.Slf4j
+@RequestScoped
+public class ContainerManagedEntityManagerProducer {
+
+    private static final String PERSISTENCE_UNIT_NAME = "xyz_PU"; // TODO inject
+    @PersistenceContext(unitName = PERSISTENCE_UNIT_NAME)
+    private EntityManager em;
+
+    @RequestScoped
+    @Produces
+    public EntityManager produce() {
+        if (log.isTraceEnabled()) {
+            log.trace(String.format("produce -> %s", em.toString()));
+        }
+        return em;
+    }
+
+    public void dispose(@Disposes EntityManager em) {
+        if (log.isTraceEnabled()) {
+            log.trace(String.format("dispose -> %s", em.toString()));
+        }
+// Only an application-managed entity manager...
+// @see javax.persistence.EntityManager.close()
+//        if (em != null && em.isOpen()) {
+//            em.close();
 //        }
-//        return this.em;
-//    }
-//
-//    public void dispose(@Disposes EntityManager em) {
-//        if (log.isTraceEnabled()) {
-//            log.trace(String.format("@Disposes -> %s", em.toString()));
-//        }
-//// Only an application-managed entity manager...
-//// @see javax.persistence.EntityManager.close()
-////        if (em != null && em.isOpen()) {
-////            em.close();
-////        }
-//    }
-//}
+    }
+}
