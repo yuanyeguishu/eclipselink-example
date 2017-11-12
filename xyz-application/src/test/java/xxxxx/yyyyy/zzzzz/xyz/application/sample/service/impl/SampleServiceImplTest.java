010f6c295075abe652586fa05846d31b8bd17c5e
Masamitsu Namioka <masamitsunamioka@gmail.com>
2017-11-12 13:12:23 +0900

    issues/#94 WIP for lombok

--- xyz-application/src/test/java/xxxxx/yyyyy/zzzzz/xyz/application/sample/service/impl/SampleServiceImplTest.java
+++ xyz-application/src/test/java/xxxxx/yyyyy/zzzzz/xyz/application/sample/service/impl/SampleServiceImplTest.java
@@ -18,6 +18,7 @@ package xxxxx.yyyyy.zzzzz.xyz.application.sample.service.impl;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
+
 import java.util.Collections;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
